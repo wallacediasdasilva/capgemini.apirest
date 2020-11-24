9f2b14d7da4de4755d603a832af508a3eb4ab0cd
wallacediasdasilva <dias_wallace@outlook.com>
2020-11-23 21:21:23 -0300

    commit

---
+++ AnimalModel.cs
@@ -0,0 +1,13 @@
+namespace RestAPI
+{
+    public class AnimalModel
+    {
+        public int Id { get; set; }
+
+        public int Qtd { get; set; }
+
+        public string Species { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Controllers/ZooController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace RestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ZooController : ControllerBase
+    {
+        public ZooController()
+        {
+        }
+
+        [HttpGet]
+        public AnimalModel Get()
+        {
+            AnimalModel animal = new AnimalModel()
+            {
+                Id = 1,
+                Name = "Leão",
+                Species = "Panthera leo",
+                Qtd = 1
+            };
+
+            return animal;
+        }
+
+        [HttpGet]
+        public List<AnimalModel> GetList()
+        {
+            return new List<AnimalModel>
+            {
+                new AnimalModel(){ Id = 1, Name = "Leão", Species = "Panthera leo", Qtd = 1},
+                new AnimalModel(){ Id = 2, Name = "Raposa", Species = "Raposa-vermelha", Qtd = 1}
+            };
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AnimalModel animal)
+        {
+            var animalPost = new AnimalModel()
+            {
+                Id = animal.Id,
+                Name = animal.Name,
+                Species = animal.Species,
+                Qtd = animal.Qtd
+            };
+
+            return Ok(animalPost);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] AnimalModel animal)
+        {
+            var animalPost = new AnimalModel()
+            {
+                Id = animal.Id,
+                Name = animal.Name,
+                Species = animal.Species,
+                Qtd = animal.Qtd
+            };
+
+            return Ok(animalPost);
+        }
+
+        [HttpPatch]
+        public IActionResult Patch([FromBody] AnimalModel animal)
+        {
+            var animalPost = new AnimalModel()
+            {
+                Name = animal.Name,
+            };
+
+            return Ok(animalPost);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
